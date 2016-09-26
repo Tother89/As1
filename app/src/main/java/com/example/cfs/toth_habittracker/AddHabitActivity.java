5f8e64d73e209dcafa16966c79917129d7d8e5ba
Dallin Toth <toth.dallin@gmail.com>
2016-09-26 16:45:56 -0600

    first stage

--- app/src/main/java/com/example/cfs/toth_habittracker/AddHabitActivity.java
+++ app/src/main/java/com/example/cfs/toth_habittracker/AddHabitActivity.java
@@ -1,7 +1,12 @@
 package com.example.cfs.toth_habittracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AddHabitActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class AddHabitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_habit);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(HabitMainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_add_habit);
+        layout.addView(textView);
     }
+
 }
--- app/src/main/java/com/example/cfs/toth_habittracker/HabitMainActivity.java
+++ app/src/main/java/com/example/cfs/toth_habittracker/HabitMainActivity.java
@@ -1,14 +1,25 @@
 package com.example.cfs.toth_habittracker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class HabitMainActivity extends AppCompatActivity {
     private static final String FILENAME = "file.sav";
-
+    public final static String EXTRA_MESSAGE ="com.example.cfs.toth_habittracker.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +29,61 @@ public class HabitMainActivity extends AppCompatActivity {
 
         Button addButton = (Button) findViewById(R.id.addbutton);
 
-        addButton.setOnClickListener(new View.OnClickListener(){
+//        addButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(this,AddHabitActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(this,AddHabitActivity.class);
-                intent.putExtra
-                startActivity(intent);
-
-
+    /** sendMessage from https://developer.android.com/training/basics/firstapp/starting-activity.html
+     *
+     * modified by toth
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, AddHabitActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    /**Code from 301 Lab lonelyTwitter**/
+    private String[] loadFromFile() {
+        ArrayList<String> tweets = new ArrayList<String>();
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            String line = in.readLine();
+            while (line != null) {
+                tweets.add(line);
+                line = in.readLine();
             }
-        });
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return tweets.toArray(new String[tweets.size()]);
+    }
+    /**Code from 301 Lab lonelyTwitter**/
+    private void saveInFile(String text, Date date) {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,
+                    Context.MODE_APPEND);
+            fos.write(new String(date.toString() + " | " + text)
+                    .getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
